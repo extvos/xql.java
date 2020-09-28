45b7617e7496b0aa1d8c16a4e5fed0d0fdb76915
Mingcai SHEN <archsh@gmail.com>
2020-09-28 18:48:45 +0800

    Updated.

--- src/main/java/org/extvos/xql/dialect/Dialect.java
+++ src/main/java/org/extvos/xql/dialect/Dialect.java
@@ -1,4 +1,19 @@
 package org.extvos.xql.dialect;
 
+import org.extvos.xql.QuerySet;
+import org.extvos.xql.Table;
+import org.extvos.xql.exception.Exception;
+
+import java.sql.ResultSet;
+
 public interface Dialect {
+//    String name();
+//
+//    String driver();
+//
+//    String declare(Table t);
+//
+//    ResultSet exec(QuerySet qs) throws Exception;
+//
+//    ResultSet query(QuerySet qs) throws Exception;
 }
